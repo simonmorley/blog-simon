8e655b9241a5cb723edea9c3d291f122a3543d91
Simon Morley <309336+simonmorley@users.noreply.github.com>
2021-09-09 21:49:53 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  url: 'https://simonmorley.co.uk',
+  pathPrefix: '/',
+  title: 'Blog by Simon Morley',
+  subtitle: 'Mutterings about triathlon, software, crypto from a shed.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Posts',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+  ],
+  author: {
+    name: 'Simon Morley',
+    photo: '/photo.jpg',
+    bio: 'Ironman, runner, cyclist & software engineer in London. Becoming a dad. CTO at a crypto exchange.',
+    contacts: {
+      email: 'simon@sumo.dev',
+      github: 'simonmorley',
+      linkedin: 'simonpmorley',
+    }
+  }
+};
---
+++ content/posts/2017-19-08---I-built-a-shed.md
@@ -0,0 +1,24 @@
+---
+title: Building a man-cave
+date: "2017-08-19T22:40:32.169Z"
+template: "post"
+draft: false
+slug: "humane-typography-in-the-digital-age"
+category: "Typography"
+tags:
+  - "Design"
+  - "Typography"
+  - "Web Development"
+description: "A work in progress"
+socialImage: "/media/42-line-bible.jpg"
+---
+
+- [The first transition](#the-first-transition)
+- [The digital age](#the-digital-age)
+- [Loss of humanity through transitions](#loss-of-humanity-through-transitions)
+- [Chasing perfection](#chasing-perfection)
+
+Building a blog.
+
+![42-line-bible.jpg](/media/42-line-bible.jpg)
+
